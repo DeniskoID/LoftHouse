c648f66b49ac5c41025ed70eca39091daa020e9a
Denis <80954428+DeniskoID@users.noreply.github.com>
2023-05-25 16:48:42 +0300

    Adds Lightbox for youtube-video & yandex-map & phone-mask and others small fixes

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const onScrollHeader = () => {
     const header = document.querySelector('header');
     const mobile = document.getElementById('header-wrapper');
-    console.log(mobile);
 
     let prevScroll = window.pageYOffset;
     let currentScroll;
@@ -31,16 +30,90 @@ document.addEventListener('DOMContentLoaded', () => {
   onScrollHeader();
 
   // ======Mobile menu========
-  const header = document.querySelector('header');
-  const navBtn = document.querySelector('.nav-icon-btn');
-  const navIcon = document.querySelector('.nav-icon');
-  const nav = document.querySelector('.bg-wrapper');
 
-  navBtn.addEventListener('click', () => {
-    navIcon.classList.toggle('nav-icon--active');
-    nav.classList.toggle('bg-wrapper--mobile');
-    header.classList.remove('fixed-header');
-    header.classList.remove('static-header');
-    document.body.classList.toggle('no-scroll');
-  });
+  const onClickBurger = () => {
+    const header = document.querySelector('header');
+    const navBtn = document.querySelector('.nav-icon-btn');
+    const navIcon = document.querySelector('.nav-icon');
+    const nav = document.querySelector('.bg-wrapper');
+
+    navBtn.addEventListener('click', () => {
+      navIcon.classList.toggle('nav-icon--active');
+      nav.classList.toggle('bg-wrapper--mobile');
+      header.classList.remove('fixed-header');
+      header.classList.remove('static-header');
+      document.documentElement.classList.toggle('no-scroll');
+    });
+  };
+
+  onClickBurger();
+
+  // ======Phone-mask========
+
+  const phoneMask = () => {
+    mask('[data-tel-input]');
+
+    const phoneInputs = document.querySelectorAll('[data-tel-input]');
+    phoneInputs.forEach((input) => {
+      input.addEventListener('input', () => {
+        if (input.value == '+') input.value = '';
+      });
+      input.addEventListener('blur', () => {
+        if (input.value == '+') input.value = '';
+      });
+    });
+  };
+
+  phoneMask();
 });
+
+// ======Yandex map========
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init() {
+  // Создание карты.
+  var map = new ymaps.Map('map', {
+    // Координаты центра карты.
+    // Порядок по умолчанию: «широта, долгота».
+    // Чтобы не определять координаты центра карты вручную,
+    // воспользуйтесь инструментом Определение координат.
+    center: [59.943543, 30.338928],
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 16,
+  });
+
+  var myPlacemark = new ymaps.Placemark(
+    [59.943543, 30.338928],
+    {
+      balloonContent: `
+				<div class="balloon">
+					<div class="balloon__address">Наб. реки Фонтанки 10-15</div>
+					<div class="balloon__contacts">
+						<a href="tel:+78121234567">+8 (812) 123-45-67</a>
+					</div>
+				</div>
+			`,
+    },
+    {
+      iconLayout: 'default#image',
+      iconImageHref: './images/map/location-pin.svg',
+      iconImageSize: [40, 40],
+      iconImageOffset: [-20, -40],
+    },
+  );
+
+  map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+
+  // map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  // map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+
+  map.geoObjects.add(myPlacemark);
+  myPlacemark.balloon.open();
+}
